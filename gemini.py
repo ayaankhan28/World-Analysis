24e1388bdc0751d115eb11920a756aa0af20b531
ayaankhan28 <98885614+ayaankhan28@users.noreply.github.com>
2024-01-08 10:51:11 +0530

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -4,36 +4,13 @@ import time
 import google.generativeai as genai
 
 
-# Used to securely store your API key
-
-
-#from IPython.display import display
-#from IPython.display import Markdown
-
-
-
-# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
-
-
-genai.configure(api_key='***')
+genai.configure(api_key='***')#https://makersuite.google.com/
 
 #####################TO CHECK AVAILABLE MODELS
 """for m in genai.list_models():
   if 'generateContent' in m.supported_generation_methods:
     print(m.name)"""
 
-########################################################################## FOR TEXT TO TEXT
-
-"""model = genai.GenerativeModel('gemini-pro')
-
-
-response = model.generate_content("give me 5 points to start youtube channel")
-print(response.text)"""
-#############FOR ANSWERSS IN CHUNKS
-"""for chunk in response:
-  print(chunk.text)
-  print("_"*80)"""
-##############################################################################
 ############################################################################## FOR  IMAGE TO TEXT
 import PIL.Image
 def anaylyze(path,query):
@@ -46,24 +23,7 @@ def anaylyze(path,query):
 
     return response.text
 ############################################################################## FOR CHATTING
-"""model = genai.GenerativeModel('gemini-pro')
-chat = model.start_chat(history=[])
-while True:
 
-    prompt = input("Enter your question: ")
-    a = time.time()
-    if(prompt == "exit"):
-        break
-    else:
-        response = chat.send_message(f"{prompt}")
-        output=chat.history
-        ##print(output)
-        print(response.text)
-        b = time.time()
-        #speech.speak(response.text)
-
-        print("Response Time: "+str(round(b-a))+" sec")"""
-#speechV2.speak(output)
 
 
 
